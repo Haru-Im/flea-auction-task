4f751a003d09a4a7170df48ef145ed93b7c5a618
Haru-Im <chalk323@naver.com>
2023-10-26 15:46:33 +0900

    setup react-navigation

---
+++ src/screens/root.stack.tsx
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export const RootStack = ({}) => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
